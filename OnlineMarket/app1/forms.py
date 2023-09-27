7c85919c90e62228257b5e74c1bab71e9307b5d0
Valentin Flavian <Flavian16Vali>
2023-09-27 09:46:03 +0300

    pot adauga imagini

--- OnlineMarket/app1/forms.py
+++ OnlineMarket/app1/forms.py
@@ -5,6 +5,7 @@ from app1.models import Item, ITEM_CHOICES
 
 
 class ItemClass(forms.ModelForm):
+
     class Meta:
         model = Item
         fields = ['name', 'price', 'type', 'image']
@@ -13,9 +14,10 @@ class ItemClass(forms.ModelForm):
             'name': TextInput(attrs={'placeholder': 'Item name', 'class': 'form-control text-white bg-dark'}),
             'price': NumberInput(attrs={'placeholder': 'Item price', 'class': 'form-control text-white bg-dark'}),
             'type': Select(attrs={'class': 'form-control text-white bg-dark'}),
-            'image': ClearableFileInput(attrs={'class': 'form-control-file'}),
+            'image': ClearableFileInput(),
         }
 
     def __init__(self, pk, *args, **kwargs):
         super(ItemClass, self).__init__(*args, **kwargs)
         self.pk = pk
+        # self.fields['image'].required = False
